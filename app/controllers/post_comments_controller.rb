6f9f4f4ea3f4f5b3eacace65f626b53360df265d
yuya banjo <yuyabanjo@yuyanoMacBook-Air.local>
2019-12-11 18:25:26 +0900

    [Start] 5-4

---
+++ app/controllers/post_comments_controller.rb
@@ -0,0 +1,5 @@
+class PostCommentsController < ApplicationController
+	def create
+		
+	end
+end
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,4 +3,6 @@ class PostImage < ApplicationRecord
 	belongs_to :user
 	attachment :image # ここを追加（_idは含めません）
 
+	has_many :post_comments, dependent: :destoy
+
 end
