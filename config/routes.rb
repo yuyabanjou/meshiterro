57a31b5d4cd1e03dc9482be810523a86e56c336f
yuya banjo <yuyabanjo@yuyanoMacBook-Air.local>
2019-12-12 14:05:10 +0900

    [Finish] 5-5

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,12 @@ Rails.application.routes.draw do
   root 'post_images#index'
 
   #PostImagesのルーティングをresourcesで一括作成（onlyで指定）
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   	resource :favorites, only: [:create, :destroy]
   	resource :post_comments, only: [:create, :destroy]
   end
+
+   resources :users, only: [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
